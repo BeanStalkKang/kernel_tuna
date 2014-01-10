75c345319b0fc04d160cde37ed4c621e6976b889
metalspring <cgibson16137@gmail.com>
2014-01-10 05:31:05 -0500

    use my default colors

--- arch/arm/mach-omap2/board-tuna-display.c
+++ arch/arm/mach-omap2/board-tuna-display.c
@@ -755,9 +755,9 @@ static struct s6e8aa0_factory_calibration_info tuna_oled_factory_info_8500k = {
 		 * maximum brightness down from 300 cd/m2 to 280.
 		 */
 		.mult = {
-			1860000000U,
-			1890000000U,
-			2100000000U,
+			2000000000U,
+			2000000000U,
+			2650000000U,
 		},
 		.rshift = 31,
 	},
@@ -807,9 +807,9 @@ static struct s6e8aa0_factory_calibration_info tuna_oled_factory_info_6500k = {
 		 * maximum brightness down from 300 cd/m2 to 280.
 		 */
 		.mult = {
-			1860000000U,
-			1890000000U,
-			2100000000U,
+			2000000000U,
+			2000000000U,
+			2650000000U,
 		},
 		.rshift = 31,
 	},
@@ -859,9 +859,9 @@ static struct s6e8aa0_factory_calibration_info tuna_oled_factory_info_sm2a2 = {
 		 * maximum brightness down from 300 cd/m2 to 280.
 		 */
 		.mult = {
-			1860000000U,
-			1890000000U,
-			2100000000U,
+			2000000000U,
+			2000000000U,
+			2650000000U,
 		},
 		.rshift = 31,
 	},
